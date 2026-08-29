e0a592d565a1469554ba00c7e7d75fa78796c1f4
Satyam Kumar <111216715+satyam1436@users.noreply.github.com>
2026-08-29 23:16:47 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import app from "./app.js";
 import connectDB from "./config/db.js";
 
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -14,4 +15,6 @@ const startServer = async () => {
     });
 };
 
+
 startServer();
+
