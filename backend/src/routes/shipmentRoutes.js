c642903373fe482ad89932768aed65d3c809d2e4
Satyam Kumar <111216715+satyam1436@users.noreply.github.com>
2026-09-01 23:49:38 +0530

    Update shipmentRoutes.js

--- backend/src/routes/shipmentRoutes.js
+++ backend/src/routes/shipmentRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { createContainer } from "../controllers/shipmentController.js";
 
+
 const router = express.Router();
 
 router.post("/", createContainer);
 
 export default router;
+
