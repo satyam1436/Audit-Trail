5473a508d3bf7d00a910667ee03b2a92d510db12
Satyam Kumar <111216715+satyam1436@users.noreply.github.com>
2026-09-06 13:26:23 +0530

    Update eventTestRoutes.js

--- backend/src/routes/eventTestRoutes.js
+++ backend/src/routes/eventTestRoutes.js
@@ -4,12 +4,9 @@ import {
     getTestEvents,
 } from "../controllers/eventTestController.js";
 
-
 const router = express.Router();
 
 router.post("/events", appendTestEvent);
 router.get("/events/:aggregateId", getTestEvents);
 
 export default router;
-
-
